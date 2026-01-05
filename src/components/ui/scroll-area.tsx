aba8ed39e33a105eb1749f8d99b719fd6b26d0bf
Jaishuk <jaishukreddy7@gmail.com>
2026-01-05 17:47:17 +0530

    scrollbar designed.

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -30,7 +30,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-primary/60 hover:bg-primary/80 transition-colors duration-200" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
